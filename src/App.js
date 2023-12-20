e322fe15b29cc1154340017a8ab76096c3c36859
Nguyen Khanh <87373594+nguyenkhanh2kpi@users.noreply.github.com>
2023-12-20 21:17:22 +0700

    fail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './component/Home';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <Home/>
+      </body>
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useReducer } from 'react'
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
